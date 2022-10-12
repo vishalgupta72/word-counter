c06d52543140f57d12dcee99cc5d8995ba450dab
Vishal Gupta <vishalnph@gmail.com>
2022-10-12 17:09:36 +0530

    Updates

---
+++ precache-manifest.122c88a11b38ff5ccf5ff8f162e00292.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "411f6567545e730da4d1",
+    "url": "/word-counter/static/css/main.57d23f36.chunk.css"
+  },
+  {
+    "revision": "411f6567545e730da4d1",
+    "url": "/word-counter/static/js/main.411f6567.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/word-counter/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/word-counter/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "772baaedbc2f8733557a",
+    "url": "/word-counter/static/js/runtime~main.772baaed.js"
+  },
+  {
+    "revision": "d80111897ab9d4f74a5cf23c23f2c2e7",
+    "url": "/word-counter/index.html"
+  }
+];
